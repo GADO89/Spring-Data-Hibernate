5676cc3631af46b642f73e750c95513f418e5109
GADO89 <gadomahmoud377@gmail.com>
2024-01-20 05:00:30 +0200

    create model Client

---
+++ src/com/spring/data/hibernate/model/Client.java
@@ -0,0 +1,44 @@
+package com.spring.data.hibernate.model;
+
+public class Client {
+
+    private Long id;
+
+    private String fullName;
+
+    private String age;
+
+    private String adress;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
