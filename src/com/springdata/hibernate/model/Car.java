66c09d56294aac08e3625d2373016186b1ca9497
GADO89 <gadomahmoud377@gmail.com>
2024-01-23 02:25:22 +0200

    create model (Car - Color)

---
+++ src/com/springdata/hibernate/model/Car.java
@@ -0,0 +1,35 @@
+package com.springdata.hibernate.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Column(name = "id")
+    private Long id;
+
+    @Column(name = "person_age")
+    private String name;
+
+    public Car() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
